6b47b2eac22add46736050f04ade703dfe72103b
Brian Sahota <bpsahota@gmail.com>
2021-07-22 11:15:11 -0400

    working on contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,18 +4,21 @@ import styled from 'styled-components'
 const Contact = () => {
     return (
         <Wrapper>
-            <div className="split-right">
-                <h1>Contact me</h1>
-                <p>talk to me</p>
-            </div>
-            <div className="split-left">
-                <div className="name-email">
-                    <input type="text" name="name" />
-                    <input type="email" name='email' />
-                </div>
-                <div className="message">
-                    <input type="text" name='message' />
-                </div>
+
+            <div className="main-container">
+                <h3>Talk to me</h3>
+                <form className='contact-form'>
+                    <label>Name</label>
+                    <input className='input-field' type="text" name='name'/>
+
+                    <label>Email</label>
+                    <input className='input-field' type="text" name='subject' />
+
+                    <label>Message</label>
+                    <textarea className='input-field' name="message"></textarea>
+
+                    <input className='submit-btn' type="submit" value='Send it' />
+                </form>
             </div>
         </Wrapper>
     )
@@ -23,5 +26,50 @@ const Contact = () => {
 
 export default Contact
 const Wrapper = styled.section`
+color: #fafafa;
+
+h3 {
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+.contact-form {
+    display: block;
+    max-width: 600px;
+    margin: 0 auto;
+    // border: 1px solid #c1c1c1;
+    padding: 15px;
+    border-radius: 5px;
+    margin-bottom: 50px;
+}
+
+.contact-form label {
+    line-height: 2.7em;
+}
+
+.contact-form textarea {
+    min-height: 100px;
+    font-size: 14px;
+}
+
+.input-field {
+    width: 100%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-radius: 5px;
+    border: 1px solid #c1c1c1;
+    font-size: 14px;
+}
+
+.submit-btn {
+    margin-top: 10px;
+    width: 100%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color: #fff;
+    background-color: black;
+    border: none;
+}
 
 `
